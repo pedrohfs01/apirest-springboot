9fbf81996bb0c44f1480606c4f7d6d60532aea9a
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2020-11-12 13:23:54 -0300

    Associação entre produtos e categorias many to many

--- src/main/java/com/pedrofernandes/apirestpedidos/config/TestConfig.java
+++ src/main/java/com/pedrofernandes/apirestpedidos/config/TestConfig.java
@@ -7,7 +7,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.pedrofernandes.apirestpedidos.domain.Categoria;
+import com.pedrofernandes.apirestpedidos.domain.Produto;
 import com.pedrofernandes.apirestpedidos.repositories.CategoriaRepository;
+import com.pedrofernandes.apirestpedidos.repositories.ProdutoRepository;
 
 @Configuration
 public class TestConfig implements CommandLineRunner{
@@ -15,13 +17,28 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	CategoriaRepository categoriaRepository;
 	
+	@Autowired
+	ProdutoRepository produtoRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
 		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null,  "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().add(p2);
+		
+		p1.getCategorias().add(cat1);
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().add(cat1);
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 }
