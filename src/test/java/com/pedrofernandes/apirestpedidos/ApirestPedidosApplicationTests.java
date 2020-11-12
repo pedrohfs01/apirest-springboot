76f3825afe7b7e537e802dbd06e6cf439e86ccaa
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2020-11-12 10:40:50 -0300

    Inicializado

---
+++ src/test/java/com/pedrofernandes/apirestpedidos/ApirestPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrofernandes.apirestpedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
