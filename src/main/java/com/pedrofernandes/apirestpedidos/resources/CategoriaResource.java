e768cf628e4e241f982ee4c19c389685a932cd1c
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2020-11-12 11:26:56 -0300

    Dominio categoria adicionado e testado!

---
+++ src/main/java/com/pedrofernandes/apirestpedidos/resources/CategoriaResource.java
@@ -0,0 +1,27 @@
+package com.pedrofernandes.apirestpedidos.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pedrofernandes.apirestpedidos.domain.Categoria;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+
+	@GetMapping
+	public List<Categoria> listar(){
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.addAll(Arrays.asList(cat1, cat2));
+		
+		return lista;
+	}
+}
