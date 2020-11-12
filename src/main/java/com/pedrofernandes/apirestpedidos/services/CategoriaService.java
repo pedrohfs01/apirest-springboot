e6df83b8e862c488e327d46e099b51cc7be3d154
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2020-11-12 13:45:23 -0300

    Ajustes no endpoint categorias, com exceção

--- src/main/java/com/pedrofernandes/apirestpedidos/services/CategoriaService.java
+++ src/main/java/com/pedrofernandes/apirestpedidos/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.pedrofernandes.apirestpedidos.domain.Categoria;
 import com.pedrofernandes.apirestpedidos.repositories.CategoriaRepository;
+import com.pedrofernandes.apirestpedidos.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,7 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Id "+id+" não encontrado. Tipo: "+Categoria.class.getName()));
 	}
 	
 }
