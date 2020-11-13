b390468f766b604446df484bcdbed9c68fd44ed5
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2020-11-13 13:21:44 -0300

    FindAll para categoria adicionado

--- src/main/java/com/pedrofernandes/apirestpedidos/resources/CategoriaResource.java
+++ src/main/java/com/pedrofernandes/apirestpedidos/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.pedrofernandes.apirestpedidos.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.pedrofernandes.apirestpedidos.domain.Categoria;
+import com.pedrofernandes.apirestpedidos.dto.CategoriaDTO;
 import com.pedrofernandes.apirestpedidos.services.CategoriaService;
 
 @RestController
@@ -28,6 +31,13 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> findById(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping()
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 	
 	@PostMapping
