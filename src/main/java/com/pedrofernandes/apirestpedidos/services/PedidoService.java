47fb9fdf03895ad931daf4df2228133a8a38a5de
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2020-11-16 15:57:53 -0300

    Adicionando toString para item pedido e pedido

--- src/main/java/com/pedrofernandes/apirestpedidos/services/PedidoService.java
+++ src/main/java/com/pedrofernandes/apirestpedidos/services/PedidoService.java
@@ -33,6 +33,9 @@ public class PedidoService {
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
 	
+	@Autowired
+	private ClienteService clienteService;
+	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Id "+id+" não encontrado. Tipo: "+Pedido.class.getName()));
@@ -42,6 +45,8 @@ public class PedidoService {
 		obj.setInstante(new Date());
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
+		
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		
 		if(obj.getPagamento() instanceof PagamentoComBoleto) {
 			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
@@ -53,10 +58,12 @@ public class PedidoService {
 		
 		for(ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
 	}
 }
