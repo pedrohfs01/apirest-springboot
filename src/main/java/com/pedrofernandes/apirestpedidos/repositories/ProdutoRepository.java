60278a4dd99181e1f4c71db8b731e54d801c8871
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2020-11-16 13:40:10 -0300

    Query com parametros para produto

--- src/main/java/com/pedrofernandes/apirestpedidos/repositories/ProdutoRepository.java
+++ src/main/java/com/pedrofernandes/apirestpedidos/repositories/ProdutoRepository.java
@@ -1,12 +1,17 @@
 package com.pedrofernandes.apirestpedidos.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.pedrofernandes.apirestpedidos.domain.Categoria;
 import com.pedrofernandes.apirestpedidos.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 	
-	
+	Page<Produto> findDistinctByNomeContainingIgnoreCaseAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 }
