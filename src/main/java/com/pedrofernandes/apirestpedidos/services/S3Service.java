a995f9287ecfdff631be8d56f6a7a84f082f105c
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2020-11-18 11:55:30 -0300

    Salvando imagem no S3

---
+++ src/main/java/com/pedrofernandes/apirestpedidos/services/S3Service.java
@@ -0,0 +1,41 @@
+package com.pedrofernandes.apirestpedidos.services;
+
+import java.io.File;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+@Service
+public class S3Service {
+
+	private Logger LOG = LoggerFactory.getLogger(S3Service.class);
+	
+	@Autowired
+	private AmazonS3 s3client;
+	
+	@Value("${s3.bucket}")
+	private String bucketName;
+	
+	public void uploadFile(String localPathFile) {
+		try {
+			File file = new File(localPathFile);
+			LOG.info("Iniciando Upload");
+			s3client.putObject(new PutObjectRequest(bucketName, "Teste", file));
+			LOG.info("Finalizado Upload");
+			
+		}catch(AmazonServiceException e) {
+			LOG.info(e.getErrorMessage());
+			LOG.info("Status Code:" +e.getErrorCode());
+		}catch(AmazonClientException e) {
+			LOG.info(e.getMessage());
+		}
+	}
+}
